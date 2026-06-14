24b9c9dd2072b0f7e8378fbeb931d40355d497ff
DhruvinSathvara07 <sathvaradhruvin05@gmail.com>
2026-06-14 13:11:48 +0530

    #

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
-      {/* <Hero /> */}
+      hello
     </main>
   );
 }
